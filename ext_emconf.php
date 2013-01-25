9d4b498b7ab1a9f446ce6817ed274fe98bbbda4b
Benjamin Kott <info@bk2k.info>
2013-01-25 17:57:59 +0100

    initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,41 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "bk2k_collection"
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'BK2K Extbase / Fluid Collection',
+    'description' => '',
+    'category' => 'plugin',
+    'author' => 'Benjamin Kott',
+    'author_email' => 'info@bk2k.info',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '',
+    'constraints' => array(
+        'depends' => array(
+            'extbase' => '6.0',
+            'fluid' => '6.0',
+            'typo3' => '6.0',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+);
+
+?>
